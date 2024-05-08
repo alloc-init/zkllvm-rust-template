2997941dfa38c61938869888d79ff2d5335b69d8
Alexander Evgin <aleasims@gmail.com>
2024-05-08 20:43:10 +0000

    Fix outdated example code
    Because a number of changes were made to assigner, current example
    is now out of limits for assigner.

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,21 @@
 
 use ark_ff::{Field, MontFp};
 use ark_pallas::Fq;
+use unroll::unroll_for_loops;
 
-fn pow(a: Fq, n: u32) -> Fq {
-    (0..n).fold(Fq::ONE, |acc, _| acc * a)
+#[unroll_for_loops]
+fn pow_2(a: Fq) -> Fq {
+    let mut acc = Fq::ONE;
+    for i in 0..2 {
+        acc = acc * a;
+    }
+
+    acc
 }
 
 #[circuit]
 pub fn field_arithmetic_example(a: Fq, b: Fq) -> Fq {
     let c = (a + b) * a + b * (a + b) * (a + b);
     const CONSTANT: Fq = MontFp!("0x12345678901234567890");
-    c * c * c / (b - a) + pow(a, 2) + CONSTANT
+    c * c * c / (b - a) + pow_2(a) + CONSTANT
 }
